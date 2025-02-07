a9bc6009cccc8b4f3a08e093d74a3c494a1d9288
PerfectVerdict <perfectverdict@protonmail.com>
2025-02-06 23:27:57 -0500

    added some styling

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 document.querySelector('#app').innerHTML = `
-    <h1>random message generator</h1>
-    <button id="getMessage" style="font-size: 20px; padding: 15px 30px; display: block; margin-left: auto; margin-right: auto;">Get Message</button>
-    <h2 id="messageDisplay"></h2>
+    <div style="
+                 background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), 
+                url('/prayinggirl.jpg');
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                height: 100vh;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;">
+    <button id="getMessage" style="background-color: #000000;
+    border: 3px solid #ffffff;
+    color: white;
+    padding: 12px 24px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 8px;
+    cursor: pointer; margin: -30px auto 20px auto; font-family: 'Arial', sans-serif; transition: background-color 0.3s ease; font-size: 30px; display: block;  auto;">Get Message</button>
+    <h1 id="messageDisplay" style="color: white; font-family: 'Arial', sans-serif; text-align: center; max-width: 500px; font-size: 30px; line-height: 40px;"></h1>
+    
 `
 document.getElementById("getMessage").addEventListener("click", async () => {
     try {
